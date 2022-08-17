7e2923d465e028b299eeeaeb8e89ab93c9aca6a2
Ahmed Said <ahmed.el.sayeed1914@gmail.com>
2022-08-17 09:43:46 +0200

    almost done with the initial rice

---
+++ sway/touche_scripts/moveDown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#source:
+#https://www.reddit.com/r/swaywm/comments/l3xer0/switch_workspaces_between_2_displays/
+output_1_name=$(swaymsg -t get_outputs --raw | jq '.[0].name' -r)
+output_2_name=$(swaymsg -t get_outputs --raw | jq '.[1].name' -r)
+workspace_on_1=$(swaymsg -t get_outputs --raw | jq '.[0].current_workspace' -r)
+workspace_on_2=$(swaymsg -t get_outputs --raw | jq '.[1].current_workspace' -r)
+swaymsg workspace $workspace_on_2
+swaymsg move workspace to output $output_1_name
---
+++ sway/touche_scripts/moveUp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#source:
+#https://www.reddit.com/r/swaywm/comments/l3xer0/switch_workspaces_between_2_displays/
+output_1_name=$(swaymsg -t get_outputs --raw | jq '.[0].name' -r)
+output_2_name=$(swaymsg -t get_outputs --raw | jq '.[1].name' -r)
+workspace_on_1=$(swaymsg -t get_outputs --raw | jq '.[0].current_workspace' -r)
+workspace_on_2=$(swaymsg -t get_outputs --raw | jq '.[1].current_workspace' -r)
+swaymsg move window to output $output_2_name
